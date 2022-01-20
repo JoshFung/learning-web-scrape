b9f80af8ba617b4513dd1d843ea68312dbcc3158
Josh Fung <joshuagyfung@gmail.com>
2022-01-19 23:23:29 -0800

    updated deprecated functions, need to fix other problems

--- tutorial1.py
+++ tutorial1.py
@@ -1,6 +1,11 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+# os.environ['PATH'] += r"D:/Programming/PycharmProjects/SeleniumDrivers/chromedriver.exe"
+driver = webdriver.Chrome("D:/Programming/PycharmProjects/SeleniumDrivers/chromedriver.exe")
 
-driver.get("https://www.memoryexpress.com/")
+driver.get("https://www.memoryexpress.com/Category/VideoCards")
+
+item_container = driver.find_element(By.CLASS_NAME, 'c-shca-icon-item_body-name')
+print(item_container)
