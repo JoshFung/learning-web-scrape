d9d719c18c91c8336324d98079cdbd411de159de
Josh Fung <joshuagyfung@gmail.com>
2022-01-22 17:10:20 -0800

    can change pages

--- tutorial1.py
+++ tutorial1.py
@@ -10,6 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # IMPORT TO TIME SCRIPT
 from datetime import datetime
+
 start_time = datetime.now()
 
 # guide im following: https://www.youtube.com/watch?v=j7VZsCCnptM
@@ -19,14 +20,20 @@ chrome_options = Options()
 chrome_options.page_load_strategy = 'eager'
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-driver.get("https://www.newegg.ca/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7708")
+driver.get("https://www.newegg.ca/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7708&PageSize=96")
 # driver.implicitly_wait(5)
+
+
+def newegg():
+    more_items = True
+    while more_items:
+        grab_items()
+        more_items = next_page()
 
 
 def grab_items():
     all_items = driver.find_elements(By.CLASS_NAME, 'item-container')
     for item in all_items:
-
         try:
             item.find_element(By.CLASS_NAME, 'item-sponsored-box')
             print("Sponsored item skipped...")
@@ -50,8 +57,21 @@ def item_details(item):
           f"SHIPPING: {item_shipping}")
 
 
-grab_items()
+def next_page():
+    page_index = driver.find_element(By.CLASS_NAME, 'list-tool-pagination-text').text.split(' ')[1]
+    current_page = page_index.split('/')[0]
+    total_pages = page_index.split('/')[1]
+    print(current_page)
+    if current_page != total_pages:
+        driver.find_element(By.XPATH, '/html/body/div[8]/div[3]/section/div/div/div[2]/div/div/div[2]/'
+                                      'div[2]/div/div[1]/div[4]/div/div/div[11]/button').click()
+        return True
+    return False
 
+
+newegg()
+
+# close chromedriver
 driver.quit()
 
-print(f"TIME: {datetime.now()-start_time}")
+print(f"TIME: {datetime.now() - start_time}")
