da9c0afbc72ff45c7cb2c572f47c43b74ac812f4
Josh Fung <joshuagyfung@gmail.com>
2022-01-23 16:15:11 -0800

    properly switched to BS4 and added WebDriverWait to properly load the page

--- script.py
+++ script.py
@@ -1,14 +1,17 @@
 import os
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 # TODO: remove this -- IMPORT TO TIME SCRIPT
 from datetime import datetime
+
 start_time = datetime.now()
 
 # guide im following: https://www.youtube.com/watch?v=j7VZsCCnptM
@@ -20,49 +23,72 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 driver.get("https://www.newegg.ca/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7708&PageSize=96")
 
-html = driver.page_source
-soup = BeautifulSoup(html, 'lxml')
-
 
 def newegg():
     more_items = True
     while more_items:
-        grab_items()
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        grab_items(soup)
         more_items = next_page()
 
 
-def grab_items():
+def grab_items(soup):
     # all_items = driver.find_elements(By.CLASS_NAME, 'item-container')
     all_items = soup.find_all('div', {'class': 'item-container'})
 
     for item in all_items:
-        try:
-            item.find_element(By.CLASS_NAME, 'item-sponsored-box')
+        # try:
+        #     item.find_element(By.CLASS_NAME, 'item-sponsored-box')
+        #     print("Sponsored item skipped...")
+        # except NoSuchElementException:
+        #     item_details(item)
+        if item.find('div', {'class': 'item-sponsored-box'}) is not None:
             print("Sponsored item skipped...")
-        except NoSuchElementException:
+        else:
             item_details(item)
 
 
 def item_details(item):
-    item_title = item.find_element(By.CLASS_NAME, 'item-title').text
-    item_normal_price = item.find_element(By.CLASS_NAME, 'price-was').text
-    if item_normal_price == '':
-        item_normal_price = item.find_element(By.CLASS_NAME, 'price-current').text
-        item_sale_price = ''
-    else:
-        item_sale_price = item.find_element(By.CLASS_NAME, 'price-current').text
+    # item_title = item.find_element(By.CLASS_NAME, 'item-title').text
+    # item_normal_price = item.find_element(By.CLASS_NAME, 'price-was').text
+    # if item_normal_price == '':
+    #     item_normal_price = item.find_element(By.CLASS_NAME, 'price-current').text
+    #     item_sale_price = ''
+    # else:
+    #     item_sale_price = item.find_element(By.CLASS_NAME, 'price-current').text
+    # item_shipping = item.find_element(By.CLASS_NAME, 'price-ship').text.split(' ')[0]
 
-    item_shipping = item.find_element(By.CLASS_NAME, 'price-ship').text.split(' ')[0]
-    print(f"ITEM: {item_title} -- "
-          f"NORMAL PRICE: {item_normal_price} -- "
-          f"SALE PRICE: {item_sale_price} -- "
-          f"SHIPPING: {item_shipping}")
+    item_title = item.find('a', {'class': 'item-title'}).text
+    item_shipping = item.find('li', {'class': 'price-ship'}).text
+
+    item_normal_price = item.find('li', {'class': 'price-was'}).text
+    if item_normal_price is not None:
+        item_sale_price = item.find('li', {'class': 'price-current'}).text
+        item_percentage_saved = item.find('li', {'class': 'price-save'}).text
+    else:
+        item_normal_price = item.find('li', {'class': 'price-current'}).text
+
+    # item_rating = item.find('i', {'class': ''})
+
+    print(f'ITEM: {item_title} -- '
+          f'NORMAL PRICE: {item_normal_price} -- '
+          f'{f"SALE PRICE: {item_sale_price} --" if item_sale_price else ""} '
+          f'SHIPPING: {item_shipping}')
 
 
 def next_page():
+    # make sure it loads in (otherwise it can throw an error)
+    try:
+        WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.CLASS_NAME, 'list-tool-pagination-text')))
+    except TimeoutException:
+        driver.quit()
+
     page_index = driver.find_element(By.CLASS_NAME, 'list-tool-pagination-text').text.split(' ')[1]
     current_page = page_index.split('/')[0]
     total_pages = page_index.split('/')[1]
+
+    # TODO: remove this eventually
     print(current_page)
     if current_page != total_pages:
         driver.find_element(By.XPATH, '/html/body/div[8]/div[3]/section/div/div/div[2]/div/div/div[2]/'
