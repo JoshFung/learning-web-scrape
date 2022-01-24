7273d0d7a15504303dddc1c3b023e301e082b8df
Josh Fung <joshuagyfung@gmail.com>
2022-01-23 17:13:14 -0800

    added delays and scraped ratings

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
-import os
 from bs4 import BeautifulSoup
+from random import randint
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 
 # TODO: remove this -- IMPORT TO TIME SCRIPT
@@ -27,6 +28,10 @@ driver.get("https://www.newegg.ca/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7
 def newegg():
     more_items = True
     while more_items:
+
+        # TODO: first delay
+        sleep(randint(1, 5))
+
         html = driver.page_source
         soup = BeautifulSoup(html, 'lxml')
         grab_items(soup)
@@ -50,14 +55,6 @@ def grab_items(soup):
 
 
 def item_details(item):
-    # item_title = item.find_element(By.CLASS_NAME, 'item-title').text
-    # item_normal_price = item.find_element(By.CLASS_NAME, 'price-was').text
-    # if item_normal_price == '':
-    #     item_normal_price = item.find_element(By.CLASS_NAME, 'price-current').text
-    #     item_sale_price = ''
-    # else:
-    #     item_sale_price = item.find_element(By.CLASS_NAME, 'price-current').text
-    # item_shipping = item.find_element(By.CLASS_NAME, 'price-ship').text.split(' ')[0]
 
     item_title = item.find('a', {'class': 'item-title'}).text
     item_shipping = item.find('li', {'class': 'price-ship'}).text
@@ -69,18 +66,24 @@ def item_details(item):
     else:
         item_normal_price = item.find('li', {'class': 'price-current'}).text
 
-    # item_rating = item.find('i', {'class': ''})
+    item_rating = item.find('i', {'class': 'rating'})
+    if item_rating is not None:
+        item_rating = item_rating['aria-label'].split(' ')[1]
+        num_ratings = item.find('span', {'class': 'item-rating-num'}).text.strip('()')
+    else:
+        item_rating = "no rating"
+        num_ratings = "no ratings"
 
-    print(f'ITEM: {item_title} -- '
-          f'NORMAL PRICE: {item_normal_price} -- '
-          f'{f"SALE PRICE: {item_sale_price} --" if item_sale_price else ""} '
-          f'SHIPPING: {item_shipping}')
+    # print(f'ITEM: {item_title} -- '
+    #       f'NORMAL PRICE: {item_normal_price} -- '
+    #       f'{f"SALE PRICE: {item_sale_price} --" if item_sale_price else ""} '
+    #       f'SHIPPING: {item_shipping}')
 
 
 def next_page():
     # make sure it loads in (otherwise it can throw an error)
     try:
-        WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.CLASS_NAME, 'list-tool-pagination-text')))
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'list-tool-pagination-text')))
     except TimeoutException:
         driver.quit()
 
@@ -90,6 +93,10 @@ def next_page():
 
     # TODO: remove this eventually
     print(current_page)
+
+    # TODO: second delay
+    sleep(randint(1, 5))
+
     if current_page != total_pages:
         driver.find_element(By.XPATH, '/html/body/div[8]/div[3]/section/div/div/div[2]/div/div/div[2]/'
                                       'div[2]/div/div[1]/div[4]/div/div/div[11]/button').click()
