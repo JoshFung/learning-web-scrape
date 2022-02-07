40c77477f02d8988ee84f850fbfe101cd6b1b478
Josh Fung <joshuagyfung@gmail.com>
2022-02-06 22:17:44 -0800

    refactored to make use of Typescript

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import ape from 'sony ape.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ape} alt="logo" />
+        <p>
+          god give me strength to keep trying react
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
