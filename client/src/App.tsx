c5f0b985d0b0d12023a466426c547e0f476d0125
Josh Fung <joshuagyfung@gmail.com>
2022-02-20 14:49:52 -0800

    api properly connected to front-end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,26 @@
+import React from 'react';
 import MainBar from './components/Header'
 import MainBody from './components/MainBody';
 import ape from 'sony ape.jpg';
 import './App.css';
 
 function App() {
+
+  const [displayMessage, setMessage] = React.useState("");
+  React.useEffect(() => {
+    const callAPI = async () => {
+      const response = await fetch("http://localhost:9000/testAPI").then(res => res.text()).then(res => setMessage(res));
+    };
+    callAPI();
+  }, []);
+
   return (
     <div className="App">
       {/* <header className="App-header"> */}
       <MainBar />
       <MainBody />
       <p>
-        recovery takes time !
+        {displayMessage}
       </p>
       {/* </header> */}
     </div>
